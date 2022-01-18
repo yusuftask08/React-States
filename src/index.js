e70b19d9b1251c0ff8ef43e36f79ab2080399775
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-01-18 11:11:30 +0300

    Sayaç Uygulaması

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './components/Counter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
