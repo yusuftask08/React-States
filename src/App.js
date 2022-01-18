2785cc6c4cd73f61f0f2d9bf7b181caf02d3e88e
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-01-18 10:50:32 +0300

    Array States

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import './App.css';
 function App() {
   const [name, setName] = useState('yusuf');
   const [age, setAge] = useState(24)
+  const [friends, setFriends] = useState(['yusuf', 'mehmet'])
   return (
     <div className="App">
       <h1> {name}</h1>
       <h1> {age}</h1>
       <button onClick={() => setName('ahmooo')}>name </button>
       <button onClick={() => setAge(2)}>age</button>
+
+      <hr />
+      <h2>Friends</h2>
+      {
+        friends.map((y, i) => <div key={i}> {y} </div>)
+      }
+      <button onClick={() => setFriends([...friends, 'ahmoo'])}>set friends</button>
+
     </div>
   );
 }
