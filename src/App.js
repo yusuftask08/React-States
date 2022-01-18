dab85a2cf081d7cda571653e6d053730e079f540
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-01-18 10:56:42 +0300

    Object States

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [name, setName] = useState('yusuf');
   const [age, setAge] = useState(24)
   const [friends, setFriends] = useState(['yusuf', 'mehmet'])
+  const [adress, setAdress] = useState({ title: 'istanbul', zip: 34000 })
+
   return (
     <div className="App">
       <h1> {name}</h1>
@@ -19,6 +21,12 @@ function App() {
         friends.map((y, i) => <div key={i}> {y} </div>)
       }
       <button onClick={() => setFriends([...friends, 'ahmoo'])}>set friends</button>
+      <hr />
+      <h2>Adress</h2>
+
+      <div > {adress.title} {adress.zip} </div>
+
+      <button onClick={() => setAdress({ ...adress, title: 'ankara', zip: 3434 })}>set adress</button>
 
     </div>
   );
