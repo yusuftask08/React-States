1630df48d4e59c07ae193d33818c589a2f682be0
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-01-18 10:36:58 +0300

    What is a state and how is it created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [name, setName] = useState('yusuf');
+  const [age, setAge] = useState(24)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> {name}</h1>
+      <h1> {age}</h1>
+      <button onClick={() => setName('ahmooo')}>name </button>
+      <button onClick={() => setAge(2)}>age</button>
     </div>
   );
 }
